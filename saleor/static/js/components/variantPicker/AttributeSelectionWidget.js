83511f4c773261cd44b07e26bcb7a8352c0403b5
Andrew Sczesnak <asczesnak@vaporeon.local>
2018-04-26 14:39:16 -0700

    Change variant picker to dropdown lists

--- saleor/static/js/components/variantPicker/AttributeSelectionWidget.js
+++ saleor/static/js/components/variantPicker/AttributeSelectionWidget.js
@@ -2,42 +2,36 @@ import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
 export default class AttributeSelectionWidget extends Component {
-
   static propTypes = {
     attribute: PropTypes.object.isRequired,
     handleChange: PropTypes.func.isRequired,
     selected: PropTypes.string
   };
 
-  handleChange = (attrPk, valuePk) => {
-    this.props.handleChange(attrPk.toString(), valuePk.toString());
+  change = (event) => {
+    this.props.selected = this.props.attribute.values[event.target.value].pk.toString();
+    this.props.handleChange(this.props.attribute.pk.toString(),
+                            this.props.attribute.values[event.target.value].pk.toString());
   }
 
   render() {
     const { attribute, selected } = this.props;
     return (
       <div className="variant-picker">
-        <div className="variant-picker__label">{attribute.name}</div>
-        <div className="btn-group" data-toggle="buttons">
-          {attribute.values.map((value, i) => {
-            const active = selected === value.pk.toString();
-            const labelClass = classNames({
-              'btn btn-secondary variant-picker__option': true,
-              'active': active
-            });
-            return (
-              <label
-                className={labelClass}
-                key={i}
-                onClick={() => this.handleChange(attribute.pk, value.pk)}>
-                <input
-                  defaultChecked={active}
-                  name={value.pk}
-                  type="radio"/>
-                {value.name}
-              </label>
-            );
-          })}
+        <div className="form-group">
+          <label className="control-label">{attribute.name}</label>
+          <select
+            className="form-control"
+            onChange={this.change}>
+            {attribute.values.map((value, i) => {
+              const active = selected === value.pk.toString();
+              return (
+                <option value={i} selected={active}>
+                  {value.name}
+                </option>
+              );
+            })};
+          </select>
         </div>
       </div>
     );
